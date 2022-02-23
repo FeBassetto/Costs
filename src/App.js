04400d57294059b5c06e260d064af9e0dbf3db35
Felipe <felipebtu9@gmail.com>
2022-02-23 07:37:16 -0300

    Start Container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 
 import RouterPages from './components/RouterPages/routerpages';
+import Container from './components/layout/Container/container';
 
 function App() {
   return (
     <div className="App">
-      <RouterPages/> 
+      <Container customClass="min-height">
+        <RouterPages/> 
+      </Container>
     </div>
   );
 }
