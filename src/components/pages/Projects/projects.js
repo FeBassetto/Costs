d838e5dc430fb3e6beefba4167f5bd02503fc791
Felipe <felipebtu9@gmail.com>
2022-02-25 07:23:16 -0300

    Finished dashboard

--- src/components/pages/Projects/projects.js
+++ src/components/pages/Projects/projects.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import styles from './projects.module.css'
 
 import Message from "../../layout/Message/message";
+import LinkButton from "../../LinkButton/linkbutton";
+import Container from "../../layout/Container/container";
 
 const Projects = () => {
 
@@ -13,9 +16,15 @@ const Projects = () => {
     }
 
     return(
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newprject" text="Criar projeto" />
+            </div>
             {message && <Message type="success" msg={message} /> }
+            <Container customClass="start">
+                <p>Projetos</p>
+            </Container>
         </div>
 
     )
