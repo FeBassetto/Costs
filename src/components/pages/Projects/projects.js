3ffb9c5a664818d110f6efbc7dd5b160bdc91eba
Felipe <felipebtu9@gmail.com>
2022-02-25 08:16:39 -0300

    Finished cards

--- src/components/pages/Projects/projects.js
+++ src/components/pages/Projects/projects.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 import styles from './projects.module.css'
 
 import Message from "../../layout/Message/message";
 import LinkButton from "../../LinkButton/linkbutton";
 import Container from "../../layout/Container/container";
+import ProjectCard from "../../project/ProjectCard/projectcard";
 
 const Projects = () => {
+
+    const [projects,setProjects] = useState([])
 
     const location = useLocation()
     let message;
@@ -15,15 +19,39 @@ const Projects = () => {
         message = location.state.message
     }
 
+    useEffect(() => {
+
+        fetch('http://localhost:5000/projects', {
+            method: 'GET',
+            headers: {
+                'Content-type' : 'application/json',
+            },
+        }).then((resp) => resp.json())
+        .then((data) => {
+            console.log(data)
+            setProjects(data)
+        })
+        .catch((err) => console.log(err))
+
+    }, [])
+
     return(
         <div className={styles.project_container}>
             <div className={styles.title_container}>
                 <h1>Meus Projetos</h1>
-                <LinkButton to="/newprject" text="Criar projeto" />
+                <LinkButton to="/newproject" text="Criar projeto" />
             </div>
             {message && <Message type="success" msg={message} /> }
             <Container customClass="start">
-                <p>Projetos</p>
+                {projects.length > 0 && 
+                    projects.map((project) => project.name && <ProjectCard 
+                    id={project.id} 
+                    name={project.name}
+                    budget={project.budget} 
+                    category={project.category.name} 
+                    key={project.id} 
+                    />)
+                }
             </Container>
         </div>
 
