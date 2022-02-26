fd784aa87cf9384d7d924000d149b0827413cd56
Felipe <felipebtu9@gmail.com>
2022-02-26 14:30:09 -0300

    Finished project edit function

--- src/components/pages/EditProject/editproject.js
+++ src/components/pages/EditProject/editproject.js
@@ -2,34 +2,97 @@ import React from "react";
 import styles from './editproject.module.css'
 
 import { useParams } from "react-router-dom";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
+
+import Loader from "../../layout/Loader/loader";
+import Container from "../../layout/Container/container";
+import ProjectForm from "../../project/ProjectForm/projectform";
 
 const EditProject = () => {
 
-    const {id} = useParams()
-    
-    const [project,setProject] = useState([])
+    const { id } = useParams()
+
+    const [project, setProject] = useState([])
+    const [showProjectForm, setShowProjectForm] = useState(false)
 
     useEffect(() => {
 
-        fetch(`http://localhost:5000/projects/${id}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application-json',
-            },
-        })
-        .then((resp) => resp.json())
-        .then((data) => {
-            setProject(data)
-        })
-        .catch(err => console.log(err))
+        setTimeout(() => {
+            fetch(`http://localhost:5000/projects/${id}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application-json',
+                },
+            })
+                .then((resp) => resp.json())
+                .then((data) => {
+                    setProject(data)
+                })
+                .catch(err => console.log(err))
+
+        }, 1000)
 
     }, [id])
 
-    return(
-        <p>
-            {project.name}
-        </p>
+    function editPost(project){
+        if(project.budget < project.cost){
+
+        }
+
+        fetch(`http://localhost:5000/projects/${project.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(project),
+        })
+        .then(resp => resp.json())
+        .then((data) => {
+            setProject(data)
+            setShowProjectForm(false)
+        })
+        .catch(err => console.log(err))
+    }
+
+
+    function toggleProjectForm(){
+        setShowProjectForm(!showProjectForm)
+    }
+
+    return (
+        <>
+            {project.name ? (
+                <div className={styles.project_details}>
+                    <Container customClass="column">
+                        <div className={styles.details_container}>
+                            <h1>Projeto: {project.name}</h1>
+                            <button onClick={toggleProjectForm} className={styles.btn} >
+                                {!showProjectForm ? 'Editar Projeto': 'Fechar'}
+                            </button>
+                            {!showProjectForm ? (
+                                <div className={styles.project_info}>  
+                                    <p>
+                                        <span>Categoria: </span> {project.category.name}
+                                    </p>
+                                    <p>
+                                        <span>Total de Orçamento:</span> {project.budget}
+                                    </p>
+                                    <p>
+                                        <span>Total Utilizado:</span> {project.cost}
+                                    </p>
+                                </div>
+                            ) : (
+                                <div className={styles.project_info}>
+                                    <ProjectForm handleSubmit={editPost} btnText="Concluir edição" projectData={project} />
+                                </div>
+                            )}
+                        </div>
+                    </Container>
+                </div>
+            ): (
+                    <Loader/>
+            )}
+        </>
     )
 
 
