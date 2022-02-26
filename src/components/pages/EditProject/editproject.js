e7126c18e4e9f579b1dfdc50b010c9f5c785924a
Felipe <felipebtu9@gmail.com>
2022-02-26 16:43:39 -0300

    Start services

--- src/components/pages/EditProject/editproject.js
+++ src/components/pages/EditProject/editproject.js
@@ -15,8 +15,10 @@ const EditProject = () => {
 
     const [project, setProject] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
+    const [showServiceForm, setShowServiceForm] = useState(false)
     const [message,setMessage] = useState()
     const [type,setType] = useState()
+    
 
     useEffect(() => {
 
@@ -38,6 +40,9 @@ const EditProject = () => {
     }, [id])
 
     function editPost(project){
+
+        setMessage('')
+
         if(project.budget < project.cost){
             setMessage('O orçamento não pode ser menor que o custo do projeto!')
             setType('error')
@@ -64,6 +69,10 @@ const EditProject = () => {
 
     function toggleProjectForm(){
         setShowProjectForm(!showProjectForm)
+    }
+
+    function toggleServiceForm(){
+        setShowServiceForm(!showServiceForm)
     }
 
     return (
@@ -95,6 +104,19 @@ const EditProject = () => {
                                 </div>
                             )}
                         </div>
+                        <div className={styles.service_form_container}>
+                                <h2>Adicione um serviço:</h2>
+                                <button onClick={toggleServiceForm} className={styles.btn} >
+                                    {!showServiceForm ? 'Adicionar serviço': 'Fechar'}
+                                </button>
+                                <div className={styles.project_info}>
+                                    {showServiceForm && <div>Formulário do serviço</div>}
+                                </div>
+                        </div>
+                        <h2>Serviços</h2>
+                        <Container customClass="start">
+                            <p>Itens de serviço</p>
+                        </Container>
                     </Container>
                 </div>
             ): (
